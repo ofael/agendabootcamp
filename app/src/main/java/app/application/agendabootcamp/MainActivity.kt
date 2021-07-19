15b197df0fc95d3ef75466de7118c724f89a9735
ofael <fael_assis@hotmail.com>
2021-07-19 17:48:48 -0300

    inserting event in the agenda

--- app/src/main/java/app/application/agendabootcamp/MainActivity.kt
+++ app/src/main/java/app/application/agendabootcamp/MainActivity.kt
@@ -1,11 +1,29 @@
 package app.application.agendabootcamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById<Button>(R.id.set_event)
+
+        btnSetEvent.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Bootcamp kotlin")
+                .putExtra(EVENT_LOCATION, "Online")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60*60*1000))
+
+            startActivity(intent)
+        }
     }
 }
